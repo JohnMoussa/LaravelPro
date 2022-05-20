ee0f44fed8b76c0cbbd66fd89b7649e9d4a4ccb8
John Moussa <kjohnmoussa@gmail.com>
2022-05-20 15:05:03 +0000

    first commit

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>About  us</title>
+
+        
+    </head>
+    <body>
+
+    <p>Step by Step we'll make it &hearts;</p>
+        
+    <p><a href="/">Page d'accueil</a></p>
+        
+    <footer>
+        <p>&copy; Copyright{{date('Y')}} </p>
+    </footer>
+
+    </body>
+</html>
